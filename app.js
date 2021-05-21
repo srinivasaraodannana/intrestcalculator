5458a140801921d205da14fa60c1370e9c19be3a
srinivasaraodannana <saikiran.dannana@gmail.com>
2021-05-21 15:50:28 +0530

    aaddd

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-    function displayDate()
-        {
+    function displayDate(){
         var date, months, month, year;
             
         var d = new Date();
@@ -8,10 +7,12 @@
         month = d.getMonth();
         year = d.getFullYear();
         date = d.getDate();
-        document.getElementById("today-date").innerHTML = date + ' ' + months[month] + ' ' + year;};
-displayDate();
-      function displayDetails()
-        {
+        document.getElementById("today-date").innerHTML = date + ' ' + months[month] + ' ' + year;
+        };
+
+        displayDate();
+
+      function displayDetails(){
          var amount1,amount,intrest,date,time,years,months,days;
                 var d = new Date();
                 amount1=document.getElementById("enterAmount").value;
@@ -35,8 +36,72 @@ displayDate();
          document.getElementById("yourIntrest").innerHTML =int2 + '/-';
          document.getElementById("yourTotal").innerHTML =total + '/-';
          document.getElementById("yourTime").innerHTML =years + "yrs" +" "+ months + "m" +"  " + days + "d";
-        };
-        document.getElementById("yourAmount").innerHTML =amount + '/-';
-        };
         
-document.getElementById("btn").addEventListener('click', displayDetails);
+        document.getElementById("yourAmount").innerHTML =amount + '/-';
+ 
+        if (total>=0)
+        {
+                var vid = document.getElementById("aud"); 
+           console.log(vid);
+                vid.play(); 
+            }
+         else{
+                var vid = document.getElementById("aud"); 
+           
+                vid.pause();
+          }
+
+    
+
+
+        };};
+
+        document.getElementById("btn").addEventListener('click', displayDetails);
+
+ 
+        //     var vid = document.getElementById("myVideo"); 
+        // vid.play()
+        // startday = new Date();
+        // clockStart = startday.getTime();
+
+        // function initStopwatch() {
+        // var myTime = new Date();
+        // return((myTime.getTime() - clockStart)/1000);
+        // }
+        // getSecs();
+        
+        // function getSecs() {
+        //     var tSecs = Math.round(initStopwatch());
+        //     var iSecs = tSecs % 60;
+        //     var iMins = Math.round((tSecs-30)/60);
+        //     var sSecs ="" + ((iSecs > 9) ? iSecs : "0" + iSecs);
+        //     var sMins ="" + ((iMins > 9) ? iMins : "0" + iMins);
+        //     document.getElementById("timespent").value = sMins+":"+sSecs;
+        //     var vid = document.getElementById("myVideo"); 
+        //     if (sSecs <=30){
+        //       if(vid.pause())
+        //       vid.play(); 
+        //     }
+        //     else{
+        //       vid.pause(); 
+              
+        //     }
+        //     this.setTimeout('getSecs()',1000);
+        
+        // }
+
+  
+
+
+         
+   
+
+
+
+
+    
+
+
+
+
+
