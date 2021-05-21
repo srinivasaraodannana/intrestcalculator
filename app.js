5e1b5aaffc814ce27eeafd67beee2a82f88c78b1
srinivasaraodannana <saikiran.dannana@gmail.com>
2021-05-21 17:09:42 +0530

    sdjhgdhjgsjsd

--- app.js
+++ app.js
@@ -22,9 +22,24 @@
                 var date2 = new Date(date);
                  years = d.getFullYear() - date2.getFullYear();
                  months = d.getMonth() - date2.getMonth();
-                 days = d.getDate() - date2.getDate();
+                 days = d.getDate() - date2.getDate() ;
+                 if(months < 0 ){
+                   years= years-1;
+                   months=12+months;
+                 }
+                 if(days < 0){
+                  months=months-1;
+                  days=days+30;
+                  if(months < 0 ){
+                    years= years-1;
+                    months=12+months;
+                  }
+                  
+                 }
                  var Difference_In_Time = d.getTime() - date2.getTime();
+                 console.log(Difference_In_Time);
                 var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+                console.log(Difference_In_Days);
                 var z = Math.trunc(Difference_In_Days);
                 time=parseInt(0.033333333*Difference_In_Days);                
         intrestCal();
@@ -35,8 +50,7 @@
          total=amount + int2;
          document.getElementById("yourIntrest").innerHTML =int2 + '/-';
          document.getElementById("yourTotal").innerHTML =total + '/-';
-         document.getElementById("yourTime").innerHTML =years + "yrs" +" "+ months + "m" +"  " + days + "d";
-        
+         document.getElementById("yourTime").innerHTML =years + "yrs" +" "+ months + "m" +"  " + days + "d"; 
         document.getElementById("yourAmount").innerHTML =amount + '/-';
  
         if (total>=0)
